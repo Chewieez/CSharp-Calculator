50473aacf12a55d7405da83515b3a9fb2e7e734b
Greg Lawrence <gregaudio@gmail.com>
2018-01-15 13:41:24 -0600

    initial commit and gitignore

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,11 @@
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int num, int num2) => num + num2;
+
+        public int Subtract(int num, int num2) => num - num2;
+    }
+
+   
+}
